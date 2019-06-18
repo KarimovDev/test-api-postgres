832cbaa390071dc5bb4857ae1cfd5973b9b7df0b
KarimovDev <kardimail@gmail.com>
2019-06-18 21:32:44 +0300

    feat: add typeORM, pg and user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        UsersModule,
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            port: 5432,
+            username: 'postgres',
+            password: 'sa',
+            database: 'db',
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: true,
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
